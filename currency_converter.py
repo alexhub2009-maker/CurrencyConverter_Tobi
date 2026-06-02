7023fedb526a789984ba1daa0f42271089cf32af
Tofu0734 <tobineu@yahoo.com>
2026-06-02 09:37:32 +0200

    initial_commit

---
+++ currency_converter.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+
+currencyTemplates = [("USD",1,"United States Dollar"),("EUR",0.86,"Euro"),("SEK",9.28,"Swedish Krona"),("RUB",72.2,"Russian Ruble") ,("ZLO",0.86,"Polish Zloty")]
+
+
+
+class Currency:
+    def __init__(self, shortName, usdEquivavlent, longName):
+        self.shortName = shortName
+        self.usdEquivavlent = usdEquivavlent
+        self.longName = longName
+
+currency = [Currency(c[0],c[1],c[2]) for c in currencyTemplates]
+
+class CurrencyConverter:
+    def __init__(self, root):
+        global currency
+        self.root = root
+        self.root.title("CurrencyConvert")
+
+        # Label
+        self.label = tk.Label(root, text="Convert from:")
+        self.label.pack(pady=10)
+
+        # Entry-Feld
+        self.entry = tk.Entry(root)
+        self.entry.pack(pady=5)
+
+        self.strvar = tk.StringVar(root)
+        self.strvar.set("United States Dollar")
+
+        self.strvar2 = tk.StringVar(root)
+        self.strvar2.set("Euro")
+
+        currencyNames = {c.longName for c in currency}
+
+        # Convert from
+        self.dropdown = tk.OptionMenu(root,self.strvar,*currencyNames)
+        self.dropdown.pack(pady=5)
+
+        # Label für Ausgabe
+        self.to_label = tk.Label(root, text="To")
+        self.to_label.pack(pady=10)
+
+        #Convert to
+        self.dropdown2 = tk.OptionMenu(root,self.strvar2,*currencyNames)
+        self.dropdown2.pack(pady=5)
+
+        # Button
+        self.button = tk.Button(root, text="Convert", command=self.convert)
+        self.button.pack(pady=10)
+
+        # Label für Ausgabe
+        self.output_label = tk.Label(root, text="")
+        self.output_label.pack(pady=10)
+
+    def convert(self):
+        which = self.strvar.get()
+        to = self.strvar2.get()
+        whichdol = todol = 0
+        for c in currency:
+            if c.longName == which:
+                whichdol = c.usdEquivavlent
+            if c.longName == to:
+                todol = c.usdEquivavlent 
+        val = self.entry.get()
+
+        dol = float(val) / float(whichdol)
+        res = dol * float(todol)
+
+        self.output_label.config(text=f"{res} {to}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    gui = CurrencyConverter(root)
+    root.mainloop()
+
